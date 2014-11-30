49096f3c18abd9af5b6468f195799b02036499d6
Kenneth Pedersen <kenneth@wangpedersen.com>
2014-11-30 19:40:54 +0100

    Comics work

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,52 @@
 var React = require('react');
 var jquery = require('jquery');
 
-var HelloMessage = React.createClass({
-  render: function() {
-    return <div>Hello {this.props.name}</div>;
-  }
+var ComicList = React.createClass({
+	getInitialState: function() {
+		return {
+			comics: [],
+			error: null
+		};
+	},
+	
+	componentDidMount: function() {
+		var component = this;
+		jquery.ajax("/api/comics")
+			.error(function(jqXHR, textStatus, errorThrown) {
+				component.setState({error: textStatus + errorThrown});
+			})
+			.success(function(data, textStatus, jqXHR) {
+				component.setState({error: null, comics: data});
+			});
+	},
+	
+	render: function() {
+		return <div>{this.state.comics.map(function(comic) {
+			return <Comic key={comic.name} comic={comic} />
+		})}</div>
+	}
 });
 
-React.render(<HelloMessage name="John" />, document.body);
+var Comic = React.createClass({
+	getInitialState: function() {
+		return {zoomed: false};
+	},
+	
+	handleClick: function(event) {
+		this.setState({zoomed: !this.state.zoomed});
+		event.preventDefault();
+	},
+	
+	render: function() {
+		var className="comic";
+		if(this.state.zoomed) className += " zoomed";
+		return <div>
+			<div><strong>{this.props.comic.name}</strong></div>
+		    <a href={this.props.comic.originalUrl} onClick={this.handleClick}><img 
+				className={className} src={this.props.comic.url} /></a>
+			<div dangerouslySetInnerHTML={{__html: this.props.comic.title}} />
+		</div>
+	}
+});
+
+React.render(<ComicList />, document.body);
